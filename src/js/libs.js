7d578ff7f31d1346832b2dec127b53f0cfee9d33
Mrrockmanish <mrrockmanish@gmail.com>
2020-10-22 08:02:20 +0300

    реализован слайдер в карточках объектов

--- src/js/libs.js
+++ src/js/libs.js
@@ -1,15 +1,17 @@
 //= ../../node_modules/jquery/dist/jquery.js
-//= ../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
+//= ../../jquery-ui-custom/jquery-ui.js
 
+//= ../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
 
 //= ../../node_modules/magnific-popup/dist/jquery.magnific-popup.js
 //= ../../bower_components/arcticModal/arcticmodal/jquery.arcticmodal.js
 
-// ../../node_modules/flickity/dist/flickity.pkgd.js
-
 //= ../../node_modules/slick-carousel/slick/slick.js
 
-//= ../../node_modules/jquery-ui/ui/widget.js
 
-//= ../../node_modules/jquery-ui/ui/widgets/mouse.js
-//= ../../node_modules/jquery-ui/ui/widgets/slider.js
+// ../../node_modules/jquery-ui/ui/widget.js
+// ../../node_modules/jquery-ui/ui/position.js
+// ../../node_modules/jquery-ui/ui/widgets/mouse.js
+// ../../node_modules/jquery-ui/ui/widgets/slider.js
+// ../../node_modules/jquery-ui/ui/unique-id.js
+// ../../node_modules/jquery-ui/ui/widgets/tooltip.js
--- src/js/main.js
+++ src/js/main.js
@@ -116,6 +116,26 @@ $(document).ready(function () {
     ]
   });
 
+  //слайдер фото в карточках
+  $('.building').find('.building-carousel').each(function (){
+    $(this).slick({
+      lazyLoad: 'ondemand',
+      fade: true,
+      slidesToScroll: 1,
+      slidesToShow: 1,
+      dots: false,
+      arrows: true,
+      prevArrow: '<div class="slick-prev slick-arrow">' +
+        '<svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '<path d="M2.82823 5.00002L5.65723 2.17202L4.24323 0.757019L0.000226974 5.00002L4.24323 9.24302L5.65723 7.82802L2.82823 5.00002Z" fill="white"/>\n' +
+        '</svg></div>',
+      nextArrow: '<div class="slick-next slick-arrow">' +
+        '<svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '<path d="M3.17177 5.00002L0.342773 2.17202L1.75677 0.757019L5.99977 5.00002L1.75677 9.24302L0.342773 7.82802L3.17177 5.00002Z" fill="white"/>\n' +
+        '</svg></div>'
+    });
+  });
+
   // универсальная функция для одиночных слайдеров
   const singleSlider = (sliderSelector) => {
     $(sliderSelector).each(function (){
@@ -171,6 +191,16 @@ $(document).ready(function () {
       // }
     });
   });
+
+  //тултипы
+  $( function() {
+    $('.tooltip').tooltip({
+      // position: {
+      //   my: "center top+30",
+      //   at: "center top",
+      // }
+    });
+  } );
 
 
 });
